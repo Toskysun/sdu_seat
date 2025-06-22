1bf3e8a9796c3ccfcabd4a34a52c59d2bb740f39
Toskysun <your-username@users.noreply.github.com>
2025-06-22 15:14:37 +0800

    Revert "fix time parse"
    This reverts commit c914092edd607a4570d746510be1e4ed900d0794.

--- src/main/kotlin/sduseat/api/Lib.kt
+++ src/main/kotlin/sduseat/api/Lib.kt
@@ -29,7 +29,6 @@ import sduseat.http.newCallResponseText
 import sduseat.http.postForm
 import sduseat.utils.GSON
 import sduseat.utils.parseString
-import sduseat.utils.TimeParser
 import java.net.SocketTimeoutException
 
 private val logger = KotlinLogging.logger {}
@@ -39,11 +38,6 @@ object Lib {
      * 存储最后一次请求的响应消息
      */
     var lastResponseMessage: String? = null
-
-    /**
-     * 存储最后解析到的预约开始时间
-     */
-    var lastParsedBookingTime: String? = null
     
     /**
      * 预约座位
@@ -91,14 +85,6 @@ object Lib {
         val status = finalJson.get("status").asInt
         val msg = finalJson.get("msg").asString
         lastResponseMessage = msg  // 存储最后的响应消息
-
-        // 尝试从响应消息中解析预约开始时间
-        val parseResult = TimeParser.parseBookingStartTime(msg)
-        if (parseResult.success) {
-            lastParsedBookingTime = parseResult.time
-            logger.info { "从服务器响应中解析到预约开始时间：${parseResult.time}" }
-        }
-
         logger.info { "预约$date ${period.startTime}-${period.endTime}时间段座位[${area.name}-${seat.name}]返回信息: $msg" }
         if (status == 1) {
             logger.info {
@@ -152,38 +138,5 @@ object Lib {
         val msg = json.get("msg").asString
         logger.info { "取消座位[${bookBean.title}]返回信息: $msg" }
         return status == 1
-    }
-
-    /**
-     * 获取系统预约开始时间
-     * 通过发送请求并解析服务器响应来获取预约开始时间
-     * @return 解析结果，包含是否成功和时间信息
-     */
-    fun getBookingStartTime(auth: IAuth): TimeParser.ParseResult {
-        return try {
-            // 这里可以发送一个特定的请求来获取系统时间信息
-            // 目前使用最后一次响应消息进行解析
-            val result = TimeParser.parseBookingStartTime(lastResponseMessage)
-
-            if (result.success) {
-                logger.info { "成功获取到系统预约开始时间：${result.time}" }
-            } else {
-                logger.warn { "无法获取系统预约开始时间：${result.message}" }
-            }
-
-            result
-        } catch (e: Exception) {
-            logger.error(e) { "获取系统预约开始时间时发生异常" }
-            TimeParser.ParseResult(false, null, "获取时间时发生异常：${e.message}")
-        }
-    }
-
-    /**
-     * 从指定的服务器响应消息中解析预约时间
-     * @param message 服务器响应消息
-     * @return 解析结果
-     */
-    fun parseBookingTimeFromMessage(message: String): TimeParser.ParseResult {
-        return TimeParser.parseBookingStartTime(message)
     }
 }
